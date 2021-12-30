eec5d16750dfb64c190fdc4a12ef49ed72cd92d9
Nikit <nikitagrigorov12@gmail.com>
2021-12-30 17:00:49 +0300

    добавлены спрайт объекты

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ scale = width // num_rays
 
 player_pos = (half_width, half_height)
 player_angle = 0
-player_speed = 2
+player_speed = 5
 
 texture_width = 1500
 texture_height = 1000
@@ -37,11 +37,11 @@ for j, row in enumerate(text_map):
 def mapping(a, b):
     return (a // tile) * tile, (b // tile) * tile
 
-
-def ray_casting(screen, player_pos, player_angle, texture):
-    ox, oy = player_pos
+def ray_casting(player, texture):
+    walls = []
+    ox, oy = player.pos
     xm, ym = mapping(ox, oy)
-    cur_angle = player_angle - half_fov
+    cur_angle = player.angle - half_fov
     for ray in range(num_rays):
         sin_a = math.sin(cur_angle)
         cos_a = math.cos(cur_angle)
@@ -64,17 +64,19 @@ def ray_casting(screen, player_pos, player_angle, texture):
                 break
             y += dy * tile
 
-        depth, offset = (depth_v, yv) if depth_v < depth_h else (depth_h, xh)
+        depth, offset, texture = (depth_v, yv, texture) if depth_v < depth_h else (depth_h, xh, texture)
         offset = int(offset) % tile
-        depth *= math.cos(player_angle - cur_angle)
-        depth = max(depth, 0.000001)
+        depth *= math.cos(player.angle - cur_angle)
+        depth = max(depth, 0.00001)
         proj_height = min(int(proj_coeff / depth), 2 * height)
 
         wall_column = texture.subsurface(offset * texture_scale, 0, texture_scale, texture_height)
         wall_column = pygame.transform.scale(wall_column, (scale, proj_height))
-        screen.blit(wall_column, (ray * scale, half_height - proj_height // 2 ))
+        wall_pos = (ray * scale, half_height - proj_height // 2)
 
+        walls.append((depth, wall_column, wall_pos))
         cur_angle += delta_angle
+    return walls
 
 class Driwing:
     def __init__(self, screen, screen_map):
@@ -82,8 +84,11 @@ class Driwing:
         self.screen_map = screen_map
         self.texture = pygame.image.load('textures/wood.jpg').convert()
 
-    def world(self, player_pos, player_angle):
-        ray_casting(screen, player.pos, player.angle, self.texture)
+    def world(self, world_objects):
+        for obj in sorted(world_objects, key=lambda n: n[0], reverse=True):
+            if obj[0]:
+                _, objectt, object_pos = obj
+                self.screen.blit(objectt, object_pos)
 
 
 class Player:
@@ -116,12 +121,55 @@ class Player:
         if keys[pygame.K_RIGHT]:
             self.angle += 0.02
 
+        self.angle %= math.pi * 2
+
+class World_Object:
+    def __init__(self, object, static, pos, shift, scale):
+        self.object = object
+        self.static = static
+        self.pos = self.x, self.y = pos[0] * tile, pos[1] * tile
+        self.shift = shift
+        self.scale = scale
+
+    def locate(self, player, walls):
+        dx, dy = self.x - player.x, self.y - player.y
+        distance = math.sqrt(dx ** 2 + dy ** 2)
+        theta = math.atan2(dy, dx)
+        gamma = theta - player.angle
+        if dx > 0 and 180 <= math.degrees(player.angle) <= 360 or dx < 0 and dy < 0:
+            gamma += math.pi * 2
+
+        delta_rays = int(gamma / delta_angle)
+        current_ray = num_rays // 2 - 1  + delta_rays
+        distance *= math.cos(half_fov - current_ray * delta_angle)
+
+        if 0 <= current_ray <= num_rays - 1 and distance < walls[current_ray][0]:
+            proj_height = int(proj_coeff / distance * scale)
+            shift = (proj_height // 2) * self.shift
+            sprite_pos = (current_ray * scale - proj_height // 2, half_height - proj_height // 2 + shift)
+            sprite = pygame.transform.scale(self.object, (proj_height, proj_height))
+            return (distance, sprite, sprite_pos)
+        else:
+            return (False,)
+
+
+
+class Sprites:
+    def __init__(self):
+        self.types = {
+            'stas': pygame.image.load('sprites/pes.png').convert_alpha()
+        }
+        self.objects = [
+            World_Object(self.types['stas'], True, (7.1, 3.1), -0.5, 0.1)
+        ]
 
 pygame.init()
 screen = pygame.display.set_mode(size)
+sprites = Sprites()
 clock = pygame.time.Clock()
 player = Player()
 driwing = Driwing(screen, world_map)
+background = pygame.image.load('textures/sky.jpg')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -129,10 +177,11 @@ while True:
     player.movement()
     screen.fill((0, 0, 0))
 
-    pygame.draw.rect(screen, (0, 0, 0), (0, 0, width, half_height))
+    pygame.draw.rect(screen, (192, 192, 192), (0, 0, width, half_height))
     pygame.draw.rect(screen, (0, 0, 0), (0, half_height, width, half_height))
+    walls = ray_casting(player, driwing.texture)
+    driwing.world(walls + [obj.locate(player, walls) for obj in sprites.objects])
 
-    driwing.world(player_pos, player_angle)
 
     pygame.display.flip()
     clock.tick(fps)
