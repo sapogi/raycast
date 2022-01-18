ae1cf980e4bd82caf7094156c1aa1ced44f76cfe
Nikit <nikitagrigorov12@gmail.com>
2022-01-18 20:03:34 +0300

    добавлена коллизия и лабиринт

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import pygame_menu
 from collections import deque
 
 size = width, height = 1200, 800
@@ -17,7 +18,7 @@ dist = num_rays / (2 * math.tan(half_fov))
 proj_coeff = 3 * dist * tile
 scale = width // num_rays
 
-player_pos = (half_width, half_height)
+player_pos = (1750, 10)
 player_angle = 0
 player_speed = 5
 
@@ -29,10 +30,12 @@ with open('map.txt', 'r', encoding='utf-8') as map:
     text_map = map.readlines()
 
 world_map = set()
+collision_walls = []
 for j, row in enumerate(text_map):
     for i, char in enumerate(row):
         if char == '1':
             world_map.add((i * tile, j * tile))
+            collision_walls.append(pygame.Rect(i * tile, j * tile, tile, tile))
 
 
 def mapping(a, b):
@@ -99,14 +102,44 @@ class Player:
         self.x, self.y = player_pos
         self.angle = player_angle
         self.sensitivity = 0.004
+        self.side = 50
+        self.rect = pygame.Rect(*player_pos, self.side, self.side)
 
     @property
     def pos(self):
         return (self.x, self.y)
 
+    def detect_collision(self, dx, dy):
+        next_rect = self.rect.copy()
+        next_rect.move_ip(dx, dy)
+        hit_indexes = next_rect.collidelistall(collision_walls)
+
+        if len(hit_indexes):
+            delta_x, delta_y = 0, 0
+            for hit_index in hit_indexes:
+                hit_rect = collision_walls[hit_index]
+                if dx > 0:
+                    delta_x += next_rect.right - hit_rect.left
+                else:
+                    delta_x += hit_rect.right - next_rect.left
+                if dy > 0:
+                    delta_y += next_rect.bottom - hit_rect.top
+                else:
+                    delta_y += hit_rect.bottom - next_rect.top
+            if abs(delta_x - delta_y) < 10:
+                dx, dy = 0, 0
+            elif delta_x > delta_y:
+                dy = 0
+            elif delta_y > delta_x:
+                dx = 0
+        self.x += dx
+        self.y += dy
+
+
     def movement(self):
         self.keys_control()
         self.mouse_control()
+        self.rect.center = self.x, self.y
         self.angle %= math.pi * 2
 
     def keys_control(self):
@@ -117,17 +150,21 @@ class Player:
             exit()
 
         if keys[pygame.K_w]:
-            self.x += player_speed * cos_a
-            self.y += player_speed * sin_a
+            dx = player_speed * cos_a
+            dy = player_speed * sin_a
+            self.detect_collision(dx, dy)
         if keys[pygame.K_s]:
-            self.x += -player_speed * cos_a
-            self.y += -player_speed * sin_a
+            dx =  -player_speed * cos_a
+            dy = -player_speed * sin_a
+            self.detect_collision(dx, dy)
         if keys[pygame.K_a]:
-            self.x += player_speed * sin_a
-            self.y += -player_speed * cos_a
+            dx = player_speed * sin_a
+            dy = -player_speed * cos_a
+            self.detect_collision(dx, dy)
         if keys[pygame.K_d]:
-            self.x += -player_speed * sin_a
-            self.y += player_speed * cos_a
+            dx = -player_speed * sin_a
+            dy = player_speed * cos_a
+            self.detect_collision(dx, dy)
         if keys[pygame.K_LEFT]:
             self.angle -= 0.02
         if keys[pygame.K_RIGHT]:
@@ -200,28 +237,36 @@ class Sprites:
                 'animation_speed': 10,
             }}
         self.objects = [
-            World_Object(self.sprite_parameters['mob'], (7.1, 3.1))
+            World_Object(self.sprite_parameters['mob'], (10.5, 30.5))
         ]
 
+def start_game():
 
+
+    sprites = Sprites()
+    clock = pygame.time.Clock()
+    player = Player()
+    driwing = Driwing(screen, world_map)
+    pygame.mouse.set_visible(False)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+        player.movement()
+        if 1000 < player.x < 1100 and 2700 < player.y < 2800:
+            exit()
+        screen.fill((0, 0, 0))
+
+        pygame.draw.rect(screen, (192, 192, 192), (0, 0, width, half_height))
+        pygame.draw.rect(screen, (0, 0, 0), (0, half_height, width, half_height))
+        walls = ray_casting(player, driwing.texture)
+        driwing.world(walls + [obj.locate(player, walls) for obj in sprites.objects])
+
+        pygame.display.flip()
+        clock.tick(fps)
 pygame.init()
 screen = pygame.display.set_mode(size)
-sprites = Sprites()
-clock = pygame.time.Clock()
-player = Player()
-driwing = Driwing(screen, world_map)
-pygame.mouse.set_visible(False)
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-    player.movement()
-    screen.fill((0, 0, 0))
-
-    pygame.draw.rect(screen, (192, 192, 192), (0, 0, width, half_height))
-    pygame.draw.rect(screen, (0, 0, 0), (0, half_height, width, half_height))
-    walls = ray_casting(player, driwing.texture)
-    driwing.world(walls + [obj.locate(player, walls) for obj in sprites.objects])
-
-    pygame.display.flip()
-    clock.tick(fps)
+menu = pygame_menu.Menu('start menu', 400, 300, theme=pygame_menu.themes.THEME_GREEN)
+menu.add.button('Play', start_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+menu.mainloop(screen)
